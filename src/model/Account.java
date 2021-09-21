f7fb05114843cd6fce7194170fafc1d4abdb6f9f
2dam <2dam@HZ375760.TartangaLH.eus>
2021-09-21 10:08:12 +0200

    Completar todas las clases y crear 3 implementaciones

--- src/model/Account.java
+++ src/model/Account.java
@@ -14,6 +14,7 @@ import java.sql.Date;
  * @author idoia
  */
 public class Account implements java.io.Serializable{
+    
     private int id;
     private String description;
     private float balance;
